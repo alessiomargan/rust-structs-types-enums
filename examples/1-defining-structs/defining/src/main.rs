0525afe9d0b81253fb70dafeb3681cc625bb9d67
Alessio Margan <alessio.margan@iit.it>
2025-11-11 19:22:48 +0100

    Defines a `Person` struct with methods.
    Introduces a `Person` struct with fields for first name, last name, and age.
    
    Implements methods `full_name` to return the full name and `is_adult` to check if the person is an adult.
    
    Includes a `main` function that creates a `Person` instance and prints its details, full name, and adult status.

--- examples/1-defining-structs/defining/src/main.rs
+++ examples/1-defining-structs/defining/src/main.rs
@@ -1,15 +1,30 @@
 
 #[derive(Debug)]
+//#[allow(dead_code)]
 struct Person {
     first_name: String,
     last_name: String,
     age: u8,
 }
 
+impl Person {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+    
+    fn is_adult(&self) -> bool {
+        self.age >= 18
+    }
+}
+
 fn main() {
-    println!("{:?}", Person {
+    let person = Person {
         first_name: "John".to_string(),
         last_name: "Doe".to_string(),
         age: 25,
-    });
+    };
+   
+    println!("{:?}", person);
+    println!("Full Name: {}", person.full_name());
+    println!("Is Adult: {}", person.is_adult());
 }
